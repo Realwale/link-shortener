39f8aac875e4250db75612c14536bd7f45467ea0
Realwale <olawaleagboola41@gmail.com>
2024-02-08 18:20:27 +0100

    create controller class

---
+++ src/main/java/com/waletech/shorturl/controller/UrlShortenerController.java
@@ -0,0 +1,12 @@
+package com.waletech.shorturl.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/link")
+public class UrlShortenerController {
+}
