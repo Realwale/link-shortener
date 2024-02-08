9b81df87cf2e9746a4f8cf2cfc66a28b465a667e
Realwale <olawaleagboola41@gmail.com>
2024-02-08 18:27:34 +0100

    add swagger configuration for api documentation

---
+++ src/main/java/com/waletech/shorturl/config/SwaggerConfig.java
@@ -0,0 +1,22 @@
+package com.waletech.shorturl.config;
+
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @io.swagger.v3.oas.annotations.info.Info(
+                title = "ShortUrl Web App",
+                version = "1.0",
+                license = @License(name = "Apache License", url = "https://www.apache.org/licenses/LICENSE-2"),
+                termsOfService = "Terms of Service"
+        ),
+
+        security = {
+                @io.swagger.v3.oas.annotations.security.SecurityRequirement(name = "Bearer Authentication", scopes = { "global" }),
+        }
+)
+public class SwaggerConfig {
+}
