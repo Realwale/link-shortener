af6bb97bcedd5eb176d71acaf3adda8222ffb34d
Realwale <olawaleagboola41@gmail.com>
2024-02-08 20:36:03 +0100

    add validation to UrlRequest

--- src/main/java/com/waletech/shorturl/dto/request/UrlRequest.java
+++ src/main/java/com/waletech/shorturl/dto/request/UrlRequest.java
@@ -1,7 +1,11 @@
 package com.waletech.shorturl.dto.request;
 
+import com.waletech.shorturl.exception.CustomException;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
@@ -10,6 +14,22 @@ import lombok.*;
 @Builder
 public class UrlRequest {
 
-    @NotEmpty(message = "Invalid url")
     private String originalUrl;
+
+
+    public void validateShortLinkRequest() throws CustomException {
+        List<String> errors = new ArrayList<>();
+
+        validateOriginalUrl(originalUrl, errors);
+
+        if (!errors.isEmpty()) {
+            throw new CustomException(String.join(", ", errors));
+        }
+    }
+
+    private static void validateOriginalUrl(String originalUrl, List<String> errors){
+        if ( originalUrl==null || originalUrl.trim().isEmpty()){
+            errors.add("URL cannot be empty.");
+        }
+    }
 }
