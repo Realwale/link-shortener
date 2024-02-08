cd6dfc72ec9d8d4afd3b298dcd144a2a1ef66ac6
Realwale <olawaleagboola41@gmail.com>
2024-02-08 16:39:48 +0100

    initialize project

---
+++ src/test/java/com/waletech/shorturl/ShorturlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waletech.shorturl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShorturlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
