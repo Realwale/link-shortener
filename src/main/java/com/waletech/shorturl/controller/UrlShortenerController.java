a482d246aa66d605da71b3d41a2f5c4d77b45e56
Realwale <olawaleagboola41@gmail.com>
2024-02-08 20:51:48 +0100

    clean up

--- src/main/java/com/waletech/shorturl/controller/UrlShortenerController.java
+++ src/main/java/com/waletech/shorturl/controller/UrlShortenerController.java
@@ -6,16 +6,11 @@ import com.waletech.shorturl.dto.response.APIResponse;
 import com.waletech.shorturl.entity.Url;
 import com.waletech.shorturl.exception.CustomException;
 import com.waletech.shorturl.service.UrlService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
 
 @RequiredArgsConstructor
 @RestController
